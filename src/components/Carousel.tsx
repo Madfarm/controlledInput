75c26d569d4d7b90e37afbd0f4c9bf019a2afcf5
AnthonyScott <anthony.scott1005@gmail.com>
2023-07-06 03:52:26 -0400

    Finish num gen

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -20,10 +20,11 @@ export default function Carousel() {
             "img": "http://placekitten.com/300/400"
         }
     ];
-    let i = 0;
-    let digits = i + 1;
+    
     const [num, setNum] = useState(null);
+    const [i, setI] = useState(0);
     const [cat, setCat] = useState(cats[i]);
+    let digits = i + 1;
 
     function genNum(){
         let ehh = "";
@@ -31,20 +32,40 @@ export default function Carousel() {
             ehh += "9";
         }
 
-
-
+        console.log(ehh);
         setNum(Math.floor(Math.random()*parseInt(ehh)));
+    }
+
+    function handleMove(mvment) {
+        if (mvment == "left") {
+            
+            if (i <= 0) {
+                setI(cats.length - 1);
+            } else {
+                setI(i - 1);
+            }
+
+        } else {
+            if (i >= cats.length - 1) {
+                setI(0);
+            } else {
+                console.log(i);
+                setI(i + 1);
+            }
+
+        }
+        setCat(cats[i]);
     }
 
     return (
         <div className='wootdoot'>
-            <div className='box'>⟵</div>
+            <div className='box' onClick={(e) => handleMove("left")}>⟵</div>
             <div className='boxMid'>
                 <h1>{cat.name}</h1>
                 <img onClick={genNum} src={cat.img} />
                 <p>{num}</p>
             </div>
-            <div className='box'>⟶</div>
+            <div className='box' onClick={(e) => handleMove("right")}>⟶</div>
         </div>
     )
 }

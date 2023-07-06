47cf2e39915a9be37b4b21f83eb1b0de148ebfc3
AnthonyScott <anthony.scott1005@gmail.com>
2023-07-06 03:12:50 -0400

    Add num gen

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Carousel() {
-    
+    const cats = [
+        {
+            "name": "Dog",
+            "img": "http://placekitten.com/200/300"
+        },
+        {
+            "name": "DogMan",
+            "img": "http://placekitten.com/300/200"
+        },
+        {
+            "name": "Kitsee",
+            "img": "http://placekitten.com/400/300"
+        },
+        {
+            "name": "Json",
+            "img": "http://placekitten.com/300/400"
+        }
+    ];
+    let i = 0;
+    let digits = i + 1;
+    const [num, setNum] = useState(null);
+    const [cat, setCat] = useState(cats[i]);
+
+    function genNum(){
+        let ehh = "";
+        for (let x = 0; x < digits ; x++) {
+            ehh += "9";
+        }
+
+
+
+        setNum(Math.floor(Math.random()*parseInt(ehh)));
+    }
 
     return (
         <div className='wootdoot'>
             <div className='box'>⟵</div>
-            <div className='box'>
-                <h1></h1>
+            <div className='boxMid'>
+                <h1>{cat.name}</h1>
+                <img onClick={genNum} src={cat.img} />
+                <p>{num}</p>
             </div>
             <div className='box'>⟶</div>
         </div>

5376a670fcb270d2587312aa3c47451fd018ce20
AnthonyScott <anthony.scott1005@gmail.com>
2023-07-06 01:33:05 -0400

    Controlled input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
 
-export default function Form() {
+export default function Form({ handleChange }) {
     return (
         <>  
             <input type="number" onChange={handleChange}></input>
+            <button type="submit">Submit</button>
         </>
     )
 }
